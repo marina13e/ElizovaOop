c963a55c3b54ac2c132d2b42544cb897da1710a6
marina13e <marina13e@mail.ru>
2021-03-02 17:04:23 +0700

    Update Vector, VectorMain

--- Vector/src/elizova/vector/Vector.java
+++ Vector/src/elizova/vector/Vector.java
@@ -3,16 +3,16 @@ package elizova.vector;
 import java.util.Arrays;
 
 public class Vector {
-    private final int n;
+    private int n;
     private double[] vector;
 
     public Vector(int n) {
-        this.n = n;
-        vector = new double[n];
-
         if (n <= 0) {
             throw new IllegalArgumentException("n must be > 0");
         }
+
+        this.n = n;
+        vector = new double[n];
 
         for (int i = 0; i < n; i++) {
             vector[i] = 0;
@@ -21,17 +21,12 @@ public class Vector {
 
     public Vector(Vector v) {
         n = v.n;
-        vector = new double[n];
         vector = v.vector;
     }
 
     public Vector(double[] array) {
         n = array.length;
-        vector = new double[n];
-
-        for (int i = 0; i < array.length; i++) {
-            vector[i] = array[i];
-        }
+        vector = array;
     }
 
     public Vector(int n, double[] array) {
@@ -49,8 +44,134 @@ public class Vector {
         }
     }
 
+    public double[] getVector() {
+        return vector;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+        vector = new double[n];
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return Arrays.toString(vector);
+    }
+
+    public int getSize() {
+        return n;
+    }
+
+    public Vector getSum(Vector vector) {
+        for (int i = 0; i < getSize(); i++) {
+            vector.vector[i] += this.vector[i];
+        }
+
+        return vector;
+    }
+
+    public Vector getDifferent(Vector vector) {
+        for (int i = 0; i < getSize(); i++) {
+            vector.vector[i] -= this.vector[i];
+        }
+
+        return vector;
+    }
+
+    public Vector getMultiplicationResult(int scalar) {
+        Vector multiplicationResult = new Vector(vector);
+
+        for (int i = 0; i < getSize(); i++) {
+            multiplicationResult.vector[i] *= scalar;
+        }
+
+        return multiplicationResult;
+    }
+
+    public Vector unfoldVector() {
+        Vector unfoldedVector = new Vector(vector);
+        final int x = -1;
+
+        for (int i = 0; i < getSize(); i++) {
+            unfoldedVector.vector[i] *= x;
+        }
+
+        return unfoldedVector;
+    }
+
+    public int getLength() {
+        return vector.length;
+    }
+
+    public double getComponent(int index) {
+        return vector[index];
+    }
+
+    public void setComponent(int index, double component) {
+        vector[index] = component;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+
+        if (object == null || object.getClass() != getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) object;
+        boolean componentsEqual = true;
+
+        for (int i = 0; i < vector.getLength(); i++) {
+            if (vector.vector[i] != this.vector[i]) {
+                componentsEqual = false;
+                break;
+            }
+        }
+
+        return n == vector.n && componentsEqual;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 7;
+        int hash = 1;
+        hash = prime * hash + Arrays.hashCode(vector);
+        hash = prime * hash + Integer.hashCode(n);
+        return hash;
+    }
+
+    public static Vector getSum(Vector vector1, Vector vector2) {
+        Vector vectorSumResult = new Vector (Math.max(vector1.n, vector2.n), vector1.vector);
+
+        for (int i = 0; i < vectorSumResult.getLength(); i++) {
+            vectorSumResult.vector[i] += vector2.vector[i];
+        }
+
+        return  vectorSumResult;
+    }
+
+    public static Vector getDifferent(Vector vector1, Vector vector2) {
+        Vector vectorSumResult = new Vector (Math.max(vector1.n, vector2.n), vector1.vector);
+
+        for (int i = 0; i < vectorSumResult.getLength(); i++) {
+            vectorSumResult.vector[i] -= vector2.vector[i];
+        }
+
+        return  vectorSumResult;
+    }
+
+    public static Vector getMultiplicationResult(int scalar, Vector vector) {
+        for (int i = 0; i < vector.getLength(); i++) {
+            vector.vector[i] *= scalar;
+        }
+
+        return vector;
     }
 }
--- Vector/src/elizova/vector_main/VectorMain.java
+++ Vector/src/elizova/vector_main/VectorMain.java
@@ -4,11 +4,15 @@ import elizova.vector.Vector;
 
 public class VectorMain {
     public static void main(String[] args) {
-        Vector vector1 = new Vector(5);
-        System.out.println("Vector 1 = " + vector1.toString());
+        try {
+            Vector vector1 = new Vector(5);
+            System.out.println("Vector 1 = " + vector1.toString());
 
-        Vector vector2 = new Vector(vector1);
-        System.out.println("Vector 2 = " + vector2.toString());
+            Vector vector2 = new Vector(vector1);
+            System.out.println("Vector 2 = " + vector2.toString());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
 
         double[] array = {0, 1, 2, 3, 4, 5, 6};
 
@@ -17,5 +21,27 @@ public class VectorMain {
 
         Vector vector4 = new Vector(10, array);
         System.out.println("Vector 4 = " + vector4.toString());
+
+        System.out.println("Sum = " + vector3.getSum(vector4));
+        System.out.println("Different = " + vector3.getDifferent(vector4));
+        System.out.println("Multiplication Result = " + vector3.getMultiplicationResult(5));
+
+        System.out.println("Unfolded Vector 4 = " + vector4.unfoldVector());
+        vector4.unfoldVector();
+
+        System.out.println("Vector 4 length = " + vector4.getLength());
+
+        System.out.println("Vector 4 component with index 4 = " + vector4.getComponent(4));
+        vector4.setComponent(4, 8);
+        System.out.println("Vector 4 component with index 4 = " + vector4.getComponent(4));
+
+        Vector vectorsSum = Vector.getSum(vector3, vector4);
+        System.out.println("Sum = " + vectorsSum);
+
+        Vector vectorsDifferent = Vector.getDifferent(vectorsSum, vector4);
+        System.out.println("Different = " + vectorsDifferent);
+
+        Vector vectorsMultiplicationResult = Vector.getMultiplicationResult(4, vectorsDifferent);
+        System.out.println("Multiplication Result = " + vectorsMultiplicationResult);
     }
 }
